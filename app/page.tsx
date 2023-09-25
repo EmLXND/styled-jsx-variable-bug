2e67fba39aea1963c2a35502bb59689cebc52eb9
emLXND <emin@candis.io>
2023-09-25 17:01:30 +0200

    add styled jsx to trigger bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,26 @@
+"use client";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  const divStyles = "width: 100%;";
+
+  return (
+    <>
+      <style jsx>
+        {`
+          div {
+            ${divStyles}
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+          }
+        `}
+      </style>
+
+      <div>
+        <h1>Some heading</h1>
+        <p>Some paragraph</p>
+      </div>
+    </>
+  );
 }
